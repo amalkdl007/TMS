62b92ad3c397dd42f74d42c781bcfdf24d418f72
amalkdl007 <93526375+amalkdl007@users.noreply.github.com>
2022-03-02 13:26:20 +0530

    patch-one

---
+++ TMS-frontend/src/component/pages/errorpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const ErrorPage = ()=> {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>ErrorPage not found</h1>
+            </div>
+            </div>
+      
+    );
+};
+export default ErrorPage
