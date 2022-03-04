27e9aa49757b44623dece64b2e6199b57019b20d
Athira-0123 <93717827+Athira-0123@users.noreply.github.com>
2022-03-05 00:40:50 +0530

    Add files via upload

---
+++ TMS-frontEnd/src/components/pages/signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Signup = ()=> {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>Signup Page</h1>
+            </div>
+            </div>
+      
+    );
+};
+export default Signup
