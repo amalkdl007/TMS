ba5da29d6f28164678f6769dc8d6825e926d59af
amalkdl007 <93526375+amalkdl007@users.noreply.github.com>
2022-03-07 10:47:24 +0530

    newChanges

--- TMS-frontend/src/App.js
+++ TMS-frontend/src/App.js
@@ -1,49 +1,33 @@
+// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Sidebar from './component/sidebar';
 import Home from "./component/pages/home";
 import About from "./component/pages/about";
 import Contact from "./component/pages/contact";
-// import Login from './component/pages/users/login';
-// import Signup from "./component/pages/users/signup";
-// import Navbar from "./component/layout/navbar";
+import Login from './component/pages/login';
+import Signup from "./component/pages/signup";
+import Navbar from "./component/layout/navbar";
 import ErrorPage from "./component/pages/errorpage";
 import Footer from "./component/pages/footer";
-// import useToken from "./component/pages/users/useToken"; 
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom"; 
-// import Navbar from './component/navbar';
 
 function App() {
-
-  // const { token, setToken } = useToken();
-
-  // if(!token) {
-  //   return (
-  //     <Router>
-  //       <Navbar token={ token }/>
-  //       <Routes>
-  //         <Route path="*" element={<Home />} />
-  //         <Route path="/login" element={<Login setToken={setToken} />} />
-  //         <Route path="/signup" element={<Signup />} />
-  //       </Routes>
-  //     </Router>
-  //   )
-  // }
   return (
     <>
     
-    <div >
+    <div className="App">
      
       <Router>
       
-      <Sidebar/>
+      <Navbar/>
 
       <Routes>
-        {/* <Route path='/sidebar' element={<Sidebar/>}/> */}
         <Route exact path= "/" element= {<Home /> }/>
         <Route exact path= "/about" element= {<About/>}/>
         <Route exact path= "/contact" element= {<Contact />}/>
+        <Route exact path= "/login" element= {<Login />}/>
+        <Route exact path= "/signup" element= {<Signup />}/>
         <Route exact path= "*" element= {<ErrorPage />}/>
       </Routes>    
 
