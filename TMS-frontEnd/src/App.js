9f75271419f97e19c68c2ec13fd6af0420abf4e3
Athira-0123 <93717827+Athira-0123@users.noreply.github.com>
2022-03-07 10:51:18 +0530

    Update App.js

--- TMS-frontEnd/src/App.js
+++ TMS-frontEnd/src/App.js
@@ -1,45 +1,19 @@
-// import logo from './logo.svg';
-import React from 'react';
+// import React from 'react';
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Home from "./component/pages/home";
-import About from "./component/pages/about";
-import Contact from "./component/pages/contact";
-import Login from './component/pages/login';
-import Signup from "./component/pages/signup";
-import Navbar from "./component/layout/navbar";
-import ErrorPage from "./component/pages/errorpage";
-import Footer from "./component/pages/footer";
-import { BrowserRouter as Router, Routes, Route,} from "react-router-dom"; 
+// import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages';
 
 function App() {
   return (
-    <>
+   
     
-    <div className="App">
+    <Router> 
      
-      <Router>
-      
-      <Navbar/>
-
-      <Routes>
-        <Route exact path= "/" element= {<Home /> }/>
-        <Route exact path= "/about" element= {<About/>}/>
-        <Route exact path= "/contact" element= {<Contact />}/>
-        <Route exact path= "/login" element= {<Login />}/>
-        <Route exact path= "/signup" element= {<Signup />}/>
-        <Route exact path= "*" element= {<ErrorPage />}/>
-      </Routes>    
-
-    
-    </Router>
-  
-   <Footer/>
-   </div>
-   </>
+     <Home/>
+  </Router>
+   
   );
 }
 
 export default App;
-
-  
